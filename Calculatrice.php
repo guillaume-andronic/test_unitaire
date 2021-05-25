be13a823a8d2bc2511f132e92f2eba1bd4bc8e91
Guillaume Andronic <guillaumeandronic@gmail.com>
2021-05-25 15:58:46 +0200

    calculatrice_php

---
+++ Calculatrice.php
@@ -0,0 +1,20 @@
+<?php
+
+class Calculatrice {    
+
+    public function addition($nb1, $nb2) {
+        return $nb1 + $nb2;
+    }
+
+    public function soustraction($nb1, $nb2) {
+        return $nb1 - $nb2;
+    }
+
+    public function multi($nb1, $nb2) {
+        return $nb1 * $nb2;
+    }
+
+    public function divi($nb1, $nb2) {
+        return $nb1 / $nb2;
+    }
+}
